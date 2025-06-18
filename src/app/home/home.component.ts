a5799c4dc47edf400744880e9f66e4f3a7670145
Emeka-JR <emekaa919@gmail.com>
2025-06-18 21:10:44 +0100

    Search/Filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,17 +14,34 @@ export class HomeComponent implements OnInit {
   currentPage = 1;
   pageSize = 6;
   totalNews = 0;
+  searchText = '';
+  filterCategory = '';
+  categories: string[] = [];
 
   constructor(private newsService: NewsService) {}
 
   ngOnInit(): void {
+    this.loadCategories();
     this.loadNews();
+  }
+
+  loadCategories(): void {
+    // This is a simple way to get categories from the news list. In a real app, you might have a categories endpoint.
+    this.newsService.listNews(1, 100).subscribe({
+      next: (response) => {
+        const allCategories = response.items.map((item: News) => item.category).filter(Boolean);
+        this.categories = Array.from(new Set(allCategories));
+      },
+      error: () => {
+        this.categories = [];
+      }
+    });
   }
 
   loadNews(page: number = 1): void {
     this.loading = true;
     this.error = null;
-    this.newsService.listNews(page, this.pageSize).subscribe({
+    this.newsService.listNews(page, this.pageSize, this.filterCategory || undefined, this.searchText || undefined).subscribe({
       next: (response) => {
         this.newsList = response.items;
         this.totalNews = response.total;
@@ -42,6 +59,18 @@ export class HomeComponent implements OnInit {
   onPageChange(page: number) {
     if (page < 1 || page > this.totalPages) return;
     this.loadNews(page);
+  }
+
+  onSearch(search: string) {
+    this.searchText = search;
+    this.currentPage = 1;
+    this.loadNews(1);
+  }
+
+  onFilterCategory(category: string) {
+    this.filterCategory = category;
+    this.currentPage = 1;
+    this.loadNews(1);
   }
 
   get totalPages(): number {
